e642f0951691eb64cafeb0eb971818c53de78336
QuyTIpSY <tllspx00134@gmail.com>
2023-02-22 14:05:42 +0700

    add

---
+++ 19T1021112.Web/Controllers/CustomerController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _19T1021112.BusinessLayers;
+using _19T1021112.DomainModels;
+
+namespace _19T1021112.Web.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private const int PAGE_SIZE = 5;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index(int page = 1, string searchValue = "")
+        {
+            int rowCount = 0;
+            var data = CommonDataService.ListOfCustomers(page, PAGE_SIZE, searchValue, out rowCount);
+            int pageCount = rowCount / PAGE_SIZE;
+            if (rowCount % PAGE_SIZE > 0)
+            {
+                pageCount += 1;
+            }
+            ViewBag.Page = page;
+            ViewBag.RowCount = rowCount;
+            ViewBag.PageCount = pageCount;
+            ViewBag.SearchValue = searchValue;
+
+            return View(data);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Bổ sung khách hàng";
+            return View("Edit");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Edit()
+        {
+            ViewBag.Title = "Cập nhật khách hàng";
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Delete()
+        {
+            ViewBag.Title = "Xóa khách hàng";
+            return View();
+        }
+    }
+}
